86ab0f2a9abcf35defcaca0df1ada6733691894a
Diego Guilherme <diegoguilherme752@oultook.com>
2022-11-08 14:10:31 -0300

    feat: event register

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -4,8 +4,28 @@ import { eventButtonDisabled } from "../../scripts/eventButtonDisabled.js"
 function eventRegister() {
     const formRegister = document.querySelector("#form-register")
     const btnRegister  = formRegister.elements[4]
+    const elements = [...formRegister.elements]
 
     eventButtonDisabled(btnRegister, formRegister)
+
+    console.log(btnRegister)
+    formRegister.addEventListener("submit", (event) => {
+        event.preventDefault()
+
+        const body = {}
+
+        elements.forEach((elem) => {
+            if (elem.tagName == "INPUT" && elem.value !== "") {
+                body[elem.id] = elem.value
+                elem.value = ""
+            }
+        })
+
+        btnRegister.innerHTML = `
+        <img class="icon-search" src="../../assets/img/spinner.png" alt="icone de procura">
+        `
+        
+    })
 }
 
 
